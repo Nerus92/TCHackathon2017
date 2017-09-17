792315fbe7da3b2a87a1e0f18bf43d30aa5f16fe
Jean-Philippe Chatain <jpchatain@softbankrobotics.com>
2017-09-16 17:57:43 -0700

    Medical issue completed

--- IsSomeoneThere/app/src/main/java/com/ist/android/issomeonethere/MedicalSituationActivity.java
+++ IsSomeoneThere/app/src/main/java/com/ist/android/issomeonethere/MedicalSituationActivity.java
@@ -1,9 +1,11 @@
 package com.ist.android.issomeonethere;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MedicalSituationActivity extends AppCompatActivity {
 
@@ -21,6 +23,42 @@ public class MedicalSituationActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 onBackPressed();
+            }
+        });
+
+        final Button b_critical = (Button) findViewById(R.id.b_critical);
+        b_critical.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        MedicalSituationActivity.this,
+                        IsRoadAccessibleActivity.class);
+                intent.putExtra("IssueType", "medical");
+                intent.putExtra("info", "Critical condition/n");
+                startActivity(intent);
+            }
+        });
+
+        final Button b_serious = (Button) findViewById(R.id.b_serious);
+        b_serious.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        MedicalSituationActivity.this,
+                        IsRoadAccessibleActivity.class);
+                intent.putExtra("IssueType", "medical");
+                intent.putExtra("info", "Serious condition/n");
+                startActivity(intent);
+            }
+        });
+
+        final Button b_stable = (Button) findViewById(R.id.b_stable);
+        b_stable.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        MedicalSituationActivity.this,
+                        IsRoadAccessibleActivity.class);
+                intent.putExtra("IssueType", "medical");
+                intent.putExtra("info", "Stable condition/n");
+                startActivity(intent);
             }
         });
     }
---
+++ IsSomeoneThere/app/src/main/java/com/ist/android/issomeonethere/WhyNoRoadActivity.java
@@ -0,0 +1,65 @@
+package com.ist.android.issomeonethere;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class WhyNoRoadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_why_no_road);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                onBackPressed();
+            }
+        });
+
+        final Button b_blocked = (Button) findViewById(R.id.b_blocked);
+        b_blocked.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        WhyNoRoadActivity.this,
+                        ChatActivity.class);
+                intent.putExtras(getIntent().getExtras());
+                intent.putExtra("info", getIntent().getExtras().getString("info").concat("Because it's blocked/n"));
+                startActivity(intent);
+            }
+        });
+
+        final Button b_flooded = (Button) findViewById(R.id.b_flooded);
+        b_flooded.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        WhyNoRoadActivity.this,
+                        ChatActivity.class);
+                intent.putExtras(getIntent().getExtras());
+                intent.putExtra("info", getIntent().getExtras().getString("info").concat("Because of flooding/n"));
+                startActivity(intent);
+            }
+        });
+
+        final Button b_other = (Button) findViewById(R.id.b_other);
+        b_other.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        WhyNoRoadActivity.this,
+                        ChatActivity.class);
+                intent.putExtras(getIntent().getExtras());
+                intent.putExtra("info", getIntent().getExtras().getString("info").concat("For other reasons/n"));
+                startActivity(intent);
+            }
+        });
+    }
+}
